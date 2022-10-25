f7a4df19ccbb8467f1f31546c9adfe180258e5bb
leesharon <leesharooon@gmail.com>
2022-10-25 17:33:00 +0300

    add new contact section added

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,20 +1,24 @@
-import { Observable } from 'rxjs'
-import { ContactService } from './../../services/contact.service'
-import { Component, OnInit } from '@angular/core'
-import { Contact } from 'src/app/models/contact.model'
+import { Observable } from "rxjs"
+import { ContactService } from "./../../services/contact.service"
+import { Component, OnInit } from "@angular/core"
+import { Contact } from "src/app/models/contact.model"
 
 @Component({
-  selector: 'contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss'],
+    selector: "contact",
+    templateUrl: "./contact.component.html",
+    styleUrls: ["./contact.component.scss"],
 })
 export class ContactComponent implements OnInit {
-  constructor(private contactService: ContactService) {}
+    constructor(private contactService: ContactService) {}
 
-  contacts$!: Observable<Contact[]>
+    contacts$!: Observable<Contact[]>
 
-  ngOnInit(): void {
-    this.contactService.query()
-    this.contacts$ = this.contactService.contacts$
-  }
+    ngOnInit(): void {
+        this.contactService.query()
+        this.contacts$ = this.contactService.contacts$
+    }
+
+    onAddContact() {
+        console.log("asd: ")
+    }
 }
