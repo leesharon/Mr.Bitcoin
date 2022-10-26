ddbe354f46eca7c2b3774788f7a3c5256c6750a1
ohayonb <ohayonzzb@gmail.com>
2022-10-26 19:31:04 +0300

    market price api call edited

--- src/app/pages/statistic/statistic.component.ts
+++ src/app/pages/statistic/statistic.component.ts
@@ -1,17 +1,20 @@
-import { BitcoinService } from "./../../services/bitcoin.service"
-import { Component, OnInit } from "@angular/core"
+import { lastValueFrom } from 'rxjs'
+import { BitcoinService } from './../../services/bitcoin.service'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-    selector: "statistic",
-    templateUrl: "./statistic.component.html",
-    styleUrls: ["./statistic.component.scss"],
+  selector: 'statistic',
+  templateUrl: './statistic.component.html',
+  styleUrls: ['./statistic.component.scss'],
 })
 export class StatisticComponent implements OnInit {
-    constructor(private bitcoinService: BitcoinService) {}
+  constructor(private bitcoinService: BitcoinService) {}
 
-    ngOnInit(): void {
-        this.bitcoinService
-            .getMarketPriceHistory()
-            .subscribe(response => console.log("response: ", response))
-    }
+  async ngOnInit(): Promise<void> {
+    const zibi = await lastValueFrom(
+      this.bitcoinService.getMarketPriceHistory()
+    )
+    console.log(`zibi:`, zibi)
+    // if (zibi) console.log(`zibi.values:`, zibi.values)
+  }
 }
--- src/app/services/bitcoin.service.ts
+++ src/app/services/bitcoin.service.ts
@@ -1,54 +1,52 @@
-import { HttpClient } from "@angular/common/http"
-import { Injectable } from "@angular/core"
-import { Observable } from "rxjs"
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { lastValueFrom, map, Observable } from 'rxjs'
 
 @Injectable({
-    providedIn: "root",
+  providedIn: 'root',
 })
 export class BitcoinService {
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    public getMarketPriceHistory(): Observable<{}> {
-        try {
-            const data = this.http.get<{ answer: {} }>(
-                `https://api.blockchain.info/charts/market-price?cors=true`
-            )
-            // .pipe(map(res => res.answer))
-            console.log("BitcoinService ~ getRate ~ data", data)
-            return data
-        } catch (err) {
-            console.log(err, "Cannot get rates")
-            throw new Error()
-        }
+  public getMarketPriceHistory(): Observable<unknown> {
+    try {
+      const apiUrl = `https://api.blockchain.info/charts/market-price?timespan=1years&format=json&cors=true`
+      const data = this.http.get(apiUrl).pipe(map((res) => res))
+
+      return data
+    } catch (err) {
+      console.log(err, 'Cannot get rates')
+      throw new Error()
     }
+  }
 
-    // public shouldAdoptPet() {
-    //     return this.http
-    //         .get<{ answer: string }>("https://yesno.wtf/api")
-    //         .pipe(map(res => res.answer))
-    // }
+  // public shouldAdoptPet() {
+  //     return this.http
+  //         .get<{ answer: string }>("https://yesno.wtf/api")
+  //         .pipe(map(res => res.answer))
+  // }
 
-    // getMarketPriceHistory() {
-    //     // return marketPriceHistory //!! delete this line on production
-    //     try {
-    //         const { data } = await axios.get(
-    //             "https://api.blockchain.info/charts/market-price?cors=true"
-    //         )
-    //         return data
-    //     } catch (err) {
-    //         console.log(err, "Cannot get market price history")
-    //     }
-    // }
+  // getMarketPriceHistory() {
+  //     // return marketPriceHistory //!! delete this line on production
+  //     try {
+  //         const { data } = await axios.get(
+  //             "https://api.blockchain.info/charts/market-price?cors=true"
+  //         )
+  //         return data
+  //     } catch (err) {
+  //         console.log(err, "Cannot get market price history")
+  //     }
+  // }
 
-    // getAvgBlockSize() {
-    //     return avgBlockSize //!! delete this line on production
-    //     try {
-    //         const { data } = await axios.get(
-    //             `https://api.blockchain.info/charts/avg-block-size?timespan=5months&format=json&cors=true`
-    //         )
-    //         return data
-    //     } catch (err) {
-    //         console.log(err, "Cannot get average block size")
-    //     }
-    // }
+  // getAvgBlockSize() {
+  //     return avgBlockSize //!! delete this line on production
+  //     try {
+  //         const { data } = await axios.get(
+  //             `https://api.blockchain.info/charts/avg-block-size?timespan=5months&format=json&cors=true`
+  //         )
+  //         return data
+  //     } catch (err) {
+  //         console.log(err, "Cannot get average block size")
+  //     }
+  // }
 }
