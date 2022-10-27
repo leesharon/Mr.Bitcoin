c9ca276b0f30c6f4355738395cf8bed2e04b848f
ohayonb <ohayonzzb@gmail.com>
2022-10-27 18:32:32 +0300

    angular-google-charts library removed

--- src/app/pages/statistic/statistic.component.ts
+++ src/app/pages/statistic/statistic.component.ts
@@ -1,38 +1,12 @@
-import { lastValueFrom } from "rxjs"
-import { BitcoinService } from "./../../services/bitcoin.service"
-import { Component, OnInit } from "@angular/core"
-import { Chart } from "src/app/models/chart.model"
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-    selector: "statistic",
-    templateUrl: "./statistic.component.html",
-    styleUrls: ["./statistic.component.scss"],
+  selector: 'statistic',
+  templateUrl: './statistic.component.html',
+  styleUrls: ['./statistic.component.scss'],
 })
 export class StatisticComponent implements OnInit {
-    constructor(private bitcoinService: BitcoinService) {}
+  constructor() {}
 
-    // marketPricesHistory: any = {
-    //     title: "Market Price History",
-    //     type: "Line",
-    //     data: [],
-    //     options: {
-    //         width: 600,
-    //         height: 500,
-    //         colors: ["#e0440e", "#e6693e", "#ec8f6e", "#f3b49f", "#f6c7b6"],
-    // backgroundColor: "#E4E4E4",
-    // is3D: true,
-    // chartArea: {
-    //     backgroundColor: {
-    //         fill: "#F4F4F4",
-    //         opacity: 100,
-    //     },
-    // },
-    // backgroundColor: {
-    //     fill: "red",
-    //     fillOpacity: 0.8,
-    // },
-    // },
-    // }
-
-    async ngOnInit() {}
+  async ngOnInit() {}
 }
