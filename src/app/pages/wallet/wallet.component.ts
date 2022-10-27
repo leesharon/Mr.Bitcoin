2d85f4afb44113544d9c4ebc77725a999af0dcf5
ohayonb <ohayonzzb@gmail.com>
2022-10-27 20:18:24 +0300

    github conflicts fixed

--- src/app/pages/wallet/wallet.component.ts
+++ src/app/pages/wallet/wallet.component.ts
@@ -1,6 +1,8 @@
 import { UserService } from '../../services/user.service'
 import { Component, OnInit } from '@angular/core'
 import { User } from 'src/app/models/user.model'
+import { BitcoinService } from 'src/app/services/bitcoin.service'
+import { ChartData } from 'src/app/models/chart-data.model'
 
 @Component({
   selector: 'wallet',
@@ -8,11 +10,19 @@ import { User } from 'src/app/models/user.model'
   styleUrls: ['./wallet.component.scss'],
 })
 export class WalletComponent implements OnInit {
-  constructor(private UserService: UserService) {}
+  constructor(
+    private UserService: UserService,
+    private bitcoinService: BitcoinService
+  ) {}
 
   user!: User
+  btcRate!: number
+  marketPricesHistory!: ChartData
 
-  ngOnInit(): void {
+  async ngOnInit() {
     this.user = this.UserService.getUser()
+    this.btcRate = (await this.bitcoinService.getRate()) as number
+    this.marketPricesHistory =
+      (await this.bitcoinService.getMarketPriceHistory()) as ChartData
   }
 }
