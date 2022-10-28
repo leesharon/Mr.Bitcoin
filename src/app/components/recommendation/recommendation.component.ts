b5832f1e2e596a4e898f47ff30fe497d993e37ea
ohayonb <ohayonzzb@gmail.com>
2022-10-28 14:17:50 +0300

    images path changed

--- src/app/components/recommendation/recommendation.component.ts
+++ src/app/components/recommendation/recommendation.component.ts
@@ -1,30 +1,30 @@
-import { Component, OnInit } from "@angular/core"
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-    selector: "recommendation",
-    templateUrl: "./recommendation.component.html",
-    styleUrls: ["./recommendation.component.scss"],
+  selector: 'recommendation',
+  templateUrl: './recommendation.component.html',
+  styleUrls: ['./recommendation.component.scss'],
 })
 export class RecommendationComponent implements OnInit {
-    constructor() {}
+  constructor() {}
 
-    recommendations = [
-        {
-            name: "Kady Baker",
-            imgSrc: "../../../assets/img/avatar-kady.jpg",
-            txt: " “The best and most efficient BTC wallet out there.”",
-        },
-        {
-            name: "Aiysha Reese",
-            imgSrc: "../../../assets/img/avatar-aiysha.jpg",
-            txt: " “Probably the best product in this market so far”",
-        },
-        {
-            name: "Arthur Clarke",
-            imgSrc: "../../../assets/img/avatar-arthur.jpg",
-            txt: " “Amazing. I feel my funds are absolutely secure.”",
-        },
-    ]
+  recommendations = [
+    {
+      name: 'Kady Baker',
+      imgSrc: './assets/img/avatar-kady.jpg',
+      txt: ' “The best and most efficient BTC wallet out there.”',
+    },
+    {
+      name: 'Aiysha Reese',
+      imgSrc: './assets/img/avatar-aiysha.jpg',
+      txt: ' “Probably the best product in this market so far”',
+    },
+    {
+      name: 'Arthur Clarke',
+      imgSrc: './assets/img/avatar-arthur.jpg',
+      txt: ' “Amazing. I feel my funds are absolutely secure.”',
+    },
+  ]
 
-    ngOnInit(): void {}
+  ngOnInit(): void {}
 }
